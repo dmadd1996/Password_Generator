d6de6b36ca5053af4dff60282f5d20fcc3b318d7
dmadd1996 <94644880+dmadd1996@users.noreply.github.com>
2022-03-24 23:15:56 -0500

    walked back to bug fixes and added pwJoin into writePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -27,9 +27,6 @@ var charsUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var charsNumeric = "0123456789"
 
 var charsSpecial = "!@#$%^&*()"
-
-var userChoice = []
-
 // THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
 var lowercaseSelect = window.confirm("Please select one or more of the following character options to be included in your password: LOWERCASE CHARACTERS?")
 
@@ -72,63 +69,70 @@ var specialSelect = window.confirm("Please select one or more of the following c
 console.log(specialSelect)
 
 
+var userChoice = []
 var definedItem = []
-var pw = []
 
 // WHEN I answer each prompt
-
-function generatePassword() {
-  function randomizer(array) {
-    var index = Math.floor(Math.random()*array.length);
-    //console.log(index);
-    var randomChar = array[index];
-    return randomChar
-  }
-    
-  if (lowercaseSelect) {
-    var userChoice = userChoice.concat(charsLowercase.split(''));
-    definedItem.push(randomizer(charsLowercase))
-  }
-  if (numericSelect) {
-    var userChoice = userChoice.concat(charsNumeric.split(''))
-    definedItem.push(randomizer(charsNumeric))
-  }
-  if (uppercaseSelect) {
-    var userChoice = userChoice.concat(charsUppercase.split(''))
-    definedItem.push(randomizer(charsUppercase))
-  }
-  if (specialSelect) {
-    var userChoice = userChoice.concat(charsSpecial.split(''))
-    definedItem.push(randomizer(charsSpecial))
-  }
-
-  for (var i = 0; i < passwordSize; i++) {
-    var character = randomizer(userChoice);
-    pw.push(character)
-  }
-
-  console.log(pw)
-
-  console.log(definedItem)
-
-  // THEN my input should be validated and at least one character type should be selected
-
-  if(definedItem.length === 0) {
-    var reload = window.confirm("You must select at least one character type. Reload and try again.");
-    if (reload) {
-      document.location.reload(true)
-    }
-  }
-  return(pwJoin)
+if (lowercaseSelect) {
+  var userChoice = userChoice.concat(charsLowercase.split(''));
+  definedItem.push(randomizer(charsLowercase))
+}
+if (numericSelect) {
+  var userChoice = userChoice.concat(charsNumeric.split(''))
+  definedItem.push(randomizer(charsNumeric))
+}
+if (uppercaseSelect) {
+  var userChoice = userChoice.concat(charsUppercase.split(''))
+  definedItem.push(randomizer(charsUppercase))
+}
+if (specialSelect) {
+  var userChoice = userChoice.concat(charsSpecial.split(''))
+  definedItem.push(randomizer(charsSpecial))
 }
 
-console.log("userChoice", userChoice)
+var pw = []
 
-var pwJoin = pw.join('')
+for (var i = 0; i < passwordSize; i++) {
+  var character = randomizer(userChoice);
+  pw.push(character)
+}
+
+console.log(pw)
+
+for (var i = 0; i < definedItem.length; i++) {
+  pw[i] = definedItem[i]
+}
+
+console.log(definedItem)
+
+// THEN my input should be validated and at least one character type should be selected
+
+if(definedItem.length === 0) {
+  var reload = window.confirm("You must select at least one character type. Reload and try again.");
+  if (reload) {
+    document.location.reload(true)
+  }
+}
+
+function randomizer(array) {
+  var index = Math.floor(Math.random()*array.length);
+  //console.log(index);
+  var randomChar = array[index];
+  return randomChar
+}
+
+
+
+//console.log("Random Chaacter: ", randomizer(charsLowercase));
+
+
+
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
+
+var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -137,11 +141,16 @@ var generateBtn = document.querySelector("#generate");
 
 var myDiv = document.getElementById("myDiv")
 
+var pwJoin = pw.join('')
+
+console.log(pwJoin)
+
 function writePassword() {
-  var password = generatePassword();
+  var password = pwJoin;
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 }
 
 // Add event listener to generate button
